b240bae0a4c04c39f4a6621f23b191f0fa260fd0
platinumpizza29 <kbelgi1@gmail.com>
2024-03-18 19:31:29 +0000

    Frameworks added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,9 @@
 import Navbar from "../components/navbar.jsx";
 import WorkHistory from "../components/WorkHistory.jsx";
+import {RiReactjsFill} from "react-icons/ri";
+import {FaGitSquare, FaHtml5, FaNode} from "react-icons/fa";
+import {SiMongodb, SiMysql, SiTailwindcss} from "react-icons/si";
+import {TbApi} from "react-icons/tb";
 
 
 export default function HomePage() {
@@ -18,7 +22,7 @@ export default function HomePage() {
                     </h2>
                     <p className={"my-4"}>Passionate in creating great experience for Digital Product</p>
                     <div className="">
-                        <button className={"btn btn-neutral mx-4"}>Talk with me!</button>
+                        <button className={"btn btn-neutral mx-4"}>Talk to me!</button>
                         <button className={"btn btn-neutral btn-outline mx-4"}>See my work</button>
                         {/*social button add later*/}
                     </div>
@@ -32,6 +36,21 @@ export default function HomePage() {
                 <div className="">
                     <h2 className={"text-4xl"}>Awards and Recognition</h2>
                     <WorkHistory/>
+                </div>
+            </section>
+            <section className={"grid grid-cols-1 md:grid-cols-2 bg-base-200 my-10 p-8 place-items-center"}>
+                <div className="">
+                    <h2 className={"text-4xl"}>Experience with variety of Languages and Frameworks</h2>
+                </div>
+                <div className="h-60 w-full grid grid-cols-4 grid-rows-2 place-items-center">
+                    <h1><RiReactjsFill/></h1>
+                    <h1><FaNode/></h1>
+                    <h1><FaGitSquare/></h1>
+                    <h1><SiTailwindcss/></h1>
+                    <h1><SiMongodb/></h1>
+                    <h1><SiMysql/></h1>
+                    <h1><FaHtml5/></h1>
+                    <h1><TbApi/></h1>
                 </div>
             </section>
         </div>
